8d444f35349fd7e0b55ea9865838e39f3f377db9
dzypherit <31358319+dzypherit@users.noreply.github.com>
2024-03-28 19:17:36 +0800

    added socials

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,11 @@ import { useState } from "react";
 import ImageDisplay from '../components/ImageDisplay';
 import SignIn from "../components/SignIn";
 import styles from "../styles/Home.module.css";
-import { Button } from '@chakra-ui/react'
+import {
+  Flex,
+  Icon,
+  Button } from '@chakra-ui/react'
+import { FaTwitter, FaDiscord } from "react-icons/fa";
 
 const Home: NextPage = () => {
   const address = useAddress();
@@ -63,6 +67,29 @@ const Home: NextPage = () => {
         <br/>
         {message && <p>{message}</p>}
       </div>
+      <Flex justifyContent="center" mt={5}>
+              <Icon
+                as={FaTwitter}
+                boxSize={8}
+                cursor="pointer"
+                onClick={() =>
+                  window.open("https://twitter.com/BasedHunks", "_blank")
+                }
+                mr={4}
+              />
+              <Icon
+                as={FaDiscord}
+                boxSize={8}
+                cursor="pointer"
+                onClick={() =>
+                  window.open(
+                    "#",
+                    "_blank"
+                  )
+                }
+                mr={4}
+              />
+            </Flex>
     </div>
   );
 };
